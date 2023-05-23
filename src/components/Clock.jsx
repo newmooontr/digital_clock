18b384331afc506eda03d3fa30eb68f7638b0987
newmoon <newmooon.tr@gmail.com>
2023-05-23 16:04:07 +0300

    clock add

---
+++ src/components/Clock.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+
+
+const Clock = () => {
+  const [clock, setClock] = useState();
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClock(date.toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+  return (
+    <div style={{color:"white"}}>
+       
+      <h1 style={{ margin: "80px" }}>DİGİTAL CLOCK</h1>
+      <h2
+        style={{
+          fontSize: "60px",
+          margin: "60px",
+          fontFamily: "Montserrat,sans-serif",
+        }}
+      >
+        {clock}
+      </h2>
+    </div>
+  );
+};
+
+export default Clock;

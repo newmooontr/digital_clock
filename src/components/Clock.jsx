7c2e3c15d2171751261686a1331ace46fdf5ad5f
newmoon <newmooon.tr@gmail.com>
2023-06-11 22:26:24 +0300

    clearInterval

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
 
-
-
 const Clock = () => {
   const [clock, setClock] = useState();
 
   useEffect(() => {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       const date = new Date();
       setClock(date.toLocaleTimeString());
     }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return (
-    <div style={{color:"#F58B1B"}}>
-       
+    <div style={{ color: "#F58B1B" }}>
       <h1 style={{ margin: "80px" }}>DİGİTAL CLOCK</h1>
       <h2
         style={{
